0eafee48a955a133d97d1fc913fd5ff5e65059a8
jhk <john3.16@snu.ac.kr>
2020-08-25 17:15:59 +0900

    CMM

--- src/tocabi_pinocchio.py
+++ src/tocabi_pinocchio.py
@@ -1,55 +1,48 @@
 #!/usr/bin/env python 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
 
 import pinocchio
 from sys import argv
 from os.path import dirname, join, abspath
 
-def talker():
-      pub = rospy.Publisher('chatter',String,queue_size=10)
-      rospy.init_node('talker', anonymous = True)
-      rate = rospy.Rate(10) #10hz
-      model = pinocchio.buildModelFromUrdf("/home/jhk/catkin_ws/src/dyros_tocabi/tocabi_description/robots/dyros_tocabi_sim.urdf")
-      data     = model.createData()
- 
-      while not rospy.is_shutdown():
-            q = pinocchio.randomConfiguration(model)
-            qdot = pinocchio.randomConfiguration(model)
-            pinocchio.forwardKinematics(model,data,q)
+qmsg = JointState()
+start = False
 
-	    CMM = pinocchio.computeCentroidalMap(model, data, q)
-            print(1)
-            print(CMM)
-            rate.sleep()
+def callback(data):
+      global qmsg
+      global start 
+      qmsg = data
+      start = True
+
+def talker():
+      rospy.init_node('tocabi_pinocchio', anonymous = False)
+      rospy.Subscriber("/tocabi/jointstates", JointState, callback)
+      pub = rospy.Publisher('/tocabi/pinocchio',Float64MultiArray,queue_size=10)
+      msg = Float64MultiArray()
+      msg.data = [0 for i in range(33*6)]
+      rate = rospy.Rate(1000) #10hz
+      model = pinocchio.buildModelFromUrdf("/home/jhk/catkin_ws/src/dyros_tocabi/tocabi_description/robots/dyros_tocabi_sim.urdf")
+      data = model.createData()
+      global start
+
+      while not rospy.is_shutdown():
+            if start:
+                  q = pinocchio.randomConfiguration(model)
+                  global qmsg
+                  for i in range(0, 33):
+                        q[i] = qmsg.position[i]
+                  CMM = pinocchio.computeCentroidalMap(model, data, q)
+                  for i in range(0, 6):
+                        for j in range(0, 33):
+                              msg.data[33*(i)+j] = CMM[i][j] 
+                  
+                  pub.publish(msg)
+                  rate.sleep()
 
 if __name__=='__main__':
       try:
             talker()
       except rospy.ROSInterruptException:
             pass
-
-# This path refers to Pinocchio source code but you can define your own directory here.
-#pinocchio_model_dir = join(dirname(dirname(str(abspath(__file__)))), "models")
-
-# You should change here to set up your own URDF file or just pass it as an argument of this example.
-#urdf_filename = pinocchio_model_dir + '/others/robots/ur_description/urdf/ur5_robot.urdf' if len(argv)<2 else argv[1]
-
-# Load the urdf model
-#model    = pinocchio.buildModelFromUrdf(urdf_filename)
-#print('model name: ' + model.name)
-
-# Create data required by the algorithms
-#data     = model.createData()
-
-# Sample a random configuration
-#q        = pinocchio.randomConfiguration(model)
-#print('q: %s' % q.T)
-
-# Perform the forward kinematics over the kinematic tree
-#pinocchio.forwardKinematics(model,data,q)
-
-# Print out the placement of each joint of the kinematic tree
-#for name, oMi in zip(model.names, data.oMi):
-#    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
-#          .format( name, *oMi.translation.T.flat )))
