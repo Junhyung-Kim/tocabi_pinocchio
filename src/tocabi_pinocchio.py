64080e78b21f57c7cd94e4e7e709a68570230a28
jhk <john3.16@snu.ac.kr>
2020-09-16 18:08:35 +0900

    ss

--- src/tocabi_pinocchio.py
+++ src/tocabi_pinocchio.py
@@ -25,42 +25,32 @@ def talker():
       global model
       pub = rospy.Publisher("/tocabi/pinocchio", model, queue_size=1)
       modelmsg.CMM = [0 for i in range(33*6)]
-      modelmsg.COR = [0 for i in range(33*6)] 
-      rate = rospy.Rate(50) #10hz
+      modelmsg.COR = [0 for i in range(33*33)] 
+      rate = rospy.Rate(1000) #10hz
       model = pinocchio.buildModelFromUrdf("/home/jhk/catkin_ws/src/dyros_tocabi/tocabi_description/robots/dyros_tocabi_sim.urdf")      
       data = model.createData()
       global start
       q = pinocchio.randomConfiguration(model)
       qdot = pinocchio.randomConfiguration(model)
-      start = True
+      
       while not rospy.is_shutdown():
             if start:
                   global qmsg
                   for i in range(0, 33):
-                        #q[i] = qmsg.position[i]
-                        q[i] = 1.0
-                        qdot[i] = 1.0
-                  #      qdot[i] = qmsg.velocity[i]
+                        q[i] = qmsg.position[i]
+                        qdot[i] = qmsg.velocity[i]
                    
                   CMM = pinocchio.computeCentroidalMap(model, data, q)
                   pinocchio.computeCoriolisMatrix(model,data,q,qdot)
-                  pinocchio.computeAllTerms(model,data,q,qdot)
-                  g = pinocchio.computeGeneralizedGravity(model,data,q)
-                  #data.nle
+                  pinocchio.
 
                   for i in range(0, 6):
                         for j in range(0, 33):
                               modelmsg.CMM[33*(i)+j] = CMM[i][j] 
 
-                  for i in range(0, 6):
+                  for i in range(0, 33):
                         for j in range(0, 33):
                               modelmsg.COR[33*(i)+j] = data.C[i][j] 
-
-                  print("Cori")
-                  print(data.C.shape)
-                  #print(np.multiply(data.C,qdot))
-                  print("aa")
-                  print(data.nle - g)
 
                   pub.publish(modelmsg)
                   rate.sleep()
