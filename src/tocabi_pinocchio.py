8e5e613bf9049290efdd047ccf3ce24f3544e8c2
jhk <john3.16@snu.ac.kr>
2020-12-03 14:21:34 +0900

    ss

--- src/tocabi_pinocchio.py
+++ src/tocabi_pinocchio.py
@@ -25,32 +25,47 @@ def talker():
       global model
       pub = rospy.Publisher("/tocabi/pinocchio", model, queue_size=1)
       modelmsg.CMM = [0 for i in range(33*6)]
-      modelmsg.COR = [0 for i in range(33*33)] 
+      modelmsg.COR = [0 for i in range(33*33)]
+      modelmsg.M = [0 for i in range(33*33)]
+      modelmsg.g = [0 for i in range(33)]
+
       rate = rospy.Rate(1000) #10hz
-      model = pinocchio.buildModelFromUrdf("/home/jhk/catkin_ws/src/dyros_tocabi/tocabi_description/robots/dyros_tocabi_sim.urdf")      
+      model = pinocchio.buildModelFromUrdf("/home/jhk/catkin_ws/src/dyros_tocabi/tocabi_description/robots/dyros_tocabi.urdf")      
       data = model.createData()
       global start
       q = pinocchio.randomConfiguration(model)
       qdot = pinocchio.randomConfiguration(model)
-      
+      qdot_t = pinocchio.randomConfiguration(model)
+      qddot_t = pinocchio.randomConfiguration(model)
+
       while not rospy.is_shutdown():
             if start:
                   global qmsg
                   for i in range(0, 33):
                         q[i] = qmsg.position[i]
                         qdot[i] = qmsg.velocity[i]
-                   
+                        qdot_t[i] = 0.0
+                        qddot_t[i] = 0.0
+
                   CMM = pinocchio.computeCentroidalMap(model, data, q)
-#                  pinocchio.computeCoriolisMatrix(model,data,q,qdot)
-                  #pinocchio.
+                  pinocchio.crba(model, data, q)
+                  pinocchio.computeCoriolisMatrix(model, data, q, qdot)
+                  pinocchio.rnea(model, data, q, qdot_t, qddot_t)
 
                   for i in range(0, 6):
                         for j in range(0, 33):
-                              modelmsg.CMM[33*(i)+j] = CMM[i][j] 
+                              modelmsg.CMM[33*(i)+j] = CMM[i][j]
 
-#                  for i in range(0, 33):
-#                        for j in range(0, 33):
-#                              modelmsg.COR[33*(i)+j] = data.C[i][j] 
+                  for i in range(0, 33):
+                        for j in range(0, 33):
+                              modelmsg.COR[33*(i)+j] = data.C[i][j] 
+
+                  for i in range(0, 33):
+                        for j in range(0, 33):
+                              modelmsg.M[33*(i)+j] = data.M[i][j] 
+
+                  for i in range(0, 33):
+                        modelmsg.g[i] = data.tau[i] 
 
                   pub.publish(modelmsg)
                   rate.sleep()
